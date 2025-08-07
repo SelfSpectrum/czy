7c6eec7dfb5f0fff20acefdd87de8f43ab2d3d6d
Celes <celestemarmar2@gmail.com>
2025-08-07 08:30:40 -0400

    Getting to tokenization

--- main.c
+++ main.c
@@ -7,7 +7,12 @@ typedef struct Token Token;
 enum TokenType {
 	TOK_INT = 0,
 	TOK_ID,
-	TOK_RETURN
+	TOK_RETURN,
+	TOK_OPENPARENTHESIS,
+	TOK_CLOSEPARENTHESIS,
+	TOK_OPENCURLYBRACES,
+	TOK_CLOSECURLYBRACES,
+	TOK_SEMICOLON
 };
 
 struct Token {
@@ -18,17 +23,30 @@ struct Token {
 int PrintToken(Token token);
 
 int main() {
-	char *czy = "int";
+	char czy[] = "int main() { return 0; }";
+	char del[] = " (){};";
 	char *ptr;
 	
-	Token token = strtok_r(czy, " ", &ptr);
-	
+	Token token = { 0 };
+	token.type = TOK_INT;
+	token.id = strtok_r(czy, del, &ptr);
+	PrintToken(token);
+	token.type = TOK_ID;
+	token.id = strtok_r(NULL, del, &ptr);
 	PrintToken(token);
 
 	return 0;
 }
 
 int PrintToken(Token token) {
-	printf("[%s]", token.id);
+	char *type[] = {"TOK_INT",
+			"TOK_ID",
+			"TOK_RETURN",
+			"TOK_OPENPARENTHESIS",
+			"TOK_CLOSEPARENTHESIS",
+			"TOK_OPENCURLYBRACES",
+			"TOK_CLOSECURLYBRACES",
+			"TOK_SEMICOLON"};
+	printf("[%s, %s] ", type[(int) token.type] ,token.id);
 	return 1;
 }
